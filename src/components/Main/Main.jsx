bff289f9340378febf75594139b399152c3de7cc
Даниил Никитин <faverus061999@gmail.com>
2021-01-26 14:38:17 +0300

    -

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
       <h1>Beoning</h1>
       <p>
         I`m Beoning.
-        <br /> My goal is to become a Senior FullStack Developer / Team Leader{" "}
+        <br /> My goal is to become a Senior FullStack Developer / Team Leader
         as quickly as possible <br /> with an understanding of the most
         important aspects of development for further implementation of my own
         ideas
