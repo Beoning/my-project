b14b2981e98d8645c23944f99ac05db2d6fe05f2
Даниил Никитин <faverus061999@gmail.com>
2021-02-10 13:38:40 +0300

    Style Past component

--- src/components/Past/Past.jsx
+++ src/components/Past/Past.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import style from "./Past.module.css";
 
 const Past = () => {
   return (
-    <div>
-      <h1>Что я изучил</h1>
-    </div>
+    <>
+      <div className={style.line}></div>
+      <img src="" alt="" />
+      <div className={style.content}>
+        <h1>What i know for today</h1>
+        <p className={style.p}></p>
+      </div>
+    </>
   );
 };
 
