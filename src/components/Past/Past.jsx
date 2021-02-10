6a817ac19eeb2c635d7f10b20c43730191b97c9e
Даниил Никитин <faverus061999@gmail.com>
2021-02-10 13:51:31 +0300

    Animations for img

--- src/components/Past/Past.jsx
+++ src/components/Past/Past.jsx
@@ -5,7 +5,11 @@ const Past = () => {
   return (
     <>
       <div className={style.line}></div>
-      <img src="" alt="" />
+      <img
+        src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
+        alt=""
+        className={style.img}
+      />
       <div className={style.content}>
         <h1>What i know for today</h1>
         <p className={style.p}></p>
