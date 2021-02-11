19eb167a24dd1b4565902abd370d4465ecd2f75a
Даниил Никитин <faverus061999@gmail.com>
2021-02-11 17:29:48 +0300

    new layout

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -3,18 +3,20 @@ import style from "./Stack.module.css";
 
 const Stack = () => {
   return (
-    <>
-      <div className={style.line}></div>
-      <img
-        src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
-        alt=""
-        className={style.img}
-      />
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <div className={style.line}></div>
+        <img
+          src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
+          alt=""
+          className={style.img}
+        />
+      </div>
       <div className={style.content}>
-        <h1>Stack of technologies I know for today</h1>
+        <h1>Stack of Technologies I know for today</h1>
         <p className={style.p}></p>
       </div>
-    </>
+    </div>
   );
 };
 
