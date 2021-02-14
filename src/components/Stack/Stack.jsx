807dbab1cb2296b726c6e63fe7712978e280eef3
Даниил Никитин <faverus061999@gmail.com>
2021-02-14 18:01:36 +0300

    Scroll + Stack styling

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -6,9 +6,69 @@ const Stack = () => {
     <div className={style.main}>
       <div className={style.content}>
         <h1>Stack of Technologies I know for today</h1>
-        <p className={style.p}></p>
       </div>
-      <div className>
+      <div className={style.hLine}></div>
+      <div className={style.description}>
+        <div className={style.wrapper}>
+          <div className={style.line}></div>
+          <img
+            src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png"
+            alt=""
+            className={style.img}
+          />
+        </div>
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
+        </div>
+      </div>
+      <div className={style.description}>
+        <div className={style.wrapper}>
+          <div className={style.line}></div>
+          <img
+            src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png"
+            alt=""
+            className={style.img}
+          />
+        </div>
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
+        </div>
+      </div>
+      <div className={style.description}>
+        <div className={style.wrapper}>
+          <div className={style.line}></div>
+          <img
+            src="https://sujanbyanjankar.com.np/wp-content/uploads/2019/09/javascript.png"
+            alt=""
+            className={style.img}
+          />
+        </div>
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
+        </div>
+      </div>
+      <div className={style.description}>
+        <div className={style.wrapper}>
+          <div className={style.line}></div>
+          <img
+            src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+            alt=""
+            className={style.img}
+          />
+        </div>
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
+        </div>
+      </div>
+      <div className={style.description}>
         <div className={style.wrapper}>
           <div className={style.line}></div>
           <img
@@ -17,47 +77,28 @@ const Stack = () => {
             className={style.img}
           />
         </div>
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
+        </div>
       </div>
-      <div>
+      <div className={style.description}>
         <div className={style.wrapper}>
           <div className={style.line}></div>
           <img
-            src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
+            src="https://redux.js.org/img/redux.svg"
             alt=""
             className={style.img}
           />
         </div>
-      </div>
-      <div>
-        <div className={style.wrapper}>
-          <div className={style.line}></div>
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
-            alt=""
-            className={style.img}
-          />
+        <div className={style.textWrap}>
+          <p className={style.text}>
+            React library: <br />
+          </p>
         </div>
       </div>
-      <div>
-        <div className={style.wrapper}>
-          <div className={style.line}></div>
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
-            alt=""
-            className={style.img}
-          />
-        </div>
-      </div>
-      <div>
-        <div className={style.wrapper}>
-          <div className={style.line}></div>
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"
-            alt=""
-            className={style.img}
-          />
-        </div>
-      </div>
+      <div className={style.hLine}></div>
     </div>
   );
 };
