926b96f7864e840670d4cbe01208e750d338c1b0
Даниил Никитин <faverus061999@gmail.com>
2021-02-14 15:19:27 +0300

    Background Aside + Style changes

--- src/Page.js
+++ src/Page.js
@@ -5,10 +5,18 @@ import Future from "./components/Future/Future";
 import Stack from "./components/Stack/Stack";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
+import style from "./Page.module.css";
 
 const Page = () => {
   return (
     <div>
+      <div className={style.backImg}>
+        <img
+          src="https://legends.yonex.com/static/images/yonex-legends_pillar_sidebar-lines-3c91abf93d23bd85ff1f7fd7222c50a8-269.png"
+          alt=""
+          className={style.background}
+        />
+      </div>
       <header>
         <Navbar />
       </header>
