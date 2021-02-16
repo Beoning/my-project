6c30d30ecfd9c40ba987a929183a35b554b66f38
Даниил Никитин <faverus061999@gmail.com>
2021-02-16 18:17:38 +0300

    Create overlay

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,6 +31,32 @@ const Navbar = () => {
         <button className={style.btn}>
           <p className={style.text}>@Beoning</p>
         </button>
+        <div className={style.overlay}>
+          <a href="https://vk.com/beoning" className={style.iconHref}>
+            <img
+              src="https://cdn0.iconfinder.com/data/icons/social-network-9/50/11-512.png"
+              alt=""
+              className={style.icon}
+            />
+          </a>
+          <a href="https://github.com/Beoning" className={style.iconHref}>
+            <img
+              src="https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg"
+              alt=""
+              className={style.icon}
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/beoningg/?hl=ru"
+            className={style.iconHref}
+          >
+            <img
+              src="https://image.flaticon.com/icons/png/512/174/174855.png"
+              alt=""
+              className={style.icon}
+            />
+          </a>
+        </div>
       </div>
     </nav>
   );
